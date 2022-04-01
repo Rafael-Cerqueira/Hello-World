c03ff59471e9e4c82ebf1dbafddf006e5082fe06
RafaCerqueira <rafael.scerqueira@hotmail.com>
2022-04-01 03:14:34 -0300

    Teste Hello World Java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package Teste.Hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int a = 65511;
+		int b = 6456;
+		
+		System.out.println("Hello World!" + (a/b) );
+	}
+
+	}
